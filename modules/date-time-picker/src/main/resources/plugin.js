d8bca52d75fc05270048eb3bec9a8fdc66993291
patrykleczycki <patryk.leczycki1@gmail.com>
2019-10-03 09:38:44 +0200

    script including plugin

---
+++ modules/date-time-picker/src/main/resources/plugin.js
@@ -0,0 +1,7 @@
+tinymce.init({
+    selector: "textarea",  // change this value according to your HTML
+    plugins: "insertdatetime",
+    menubar: "insert",
+    toolbar: "insertdatetime",
+    insertdatetime_dateformat: "%Y-%m-%d"
+});
